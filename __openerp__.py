3c74285c315c786a8f0e5dda6f112b1139113861
Bernard Delhez <bernarddelhez@gmail.com>
2016-08-19 15:06:04 +0200

    update version to 9

--- __openerp__.py
+++ __openerp__.py
@@ -1,6 +1,6 @@
 ﻿{
     'name': "Attachments on leaves",
-    'version': '1.0',
+    'version': '9.0.1.0',
     'depends': ['hr_holidays'],
     'author': "Bernard DELHEZ, AbAKUS it-solutions SARL",
     'website': "http://www.abakusitsolutions.eu",
